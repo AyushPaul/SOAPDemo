4284b6ac36df5635b2de970260bd99b30d60da07
Ayush Paul <ayushpaul2002@gmail.com>
2023-11-23 00:57:24 +0530

    Implemented XML Parsing

--- src/main/java/com/ayush/soapdemo/controller/SoapController.java
+++ src/main/java/com/ayush/soapdemo/controller/SoapController.java
@@ -7,11 +7,20 @@ import com.ayush.soapdemo.util.SoapConnectionUtil;
 import jakarta.xml.soap.SOAPException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -78,6 +87,63 @@ public class SoapController {
             return "Error reading the uploaded WSDL file: " + e.getMessage();
         } catch (Exception e) {
             return "Error generating mock response: " + e.getMessage();
+        }
+    }
+
+    @PostMapping(value = "/parseXml")
+    public Map<String, Object> parseXml(
+            @RequestParam("xmlData") String xmlData,
+            @RequestParam("parameters") List<String> parameters) {
+        Map<String, Object> result = new HashMap<>();
+
+        try {
+            // Create a DocumentBuilder
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+
+            // Parse the XML string
+            Document document = builder.parse(new ByteArrayInputStream(xmlData.getBytes()));
+
+            // Traverse the XML document and filter specified tags
+            NodeList childNodes = document.getDocumentElement().getChildNodes();
+            for (int i = 0; i < childNodes.getLength(); i++) {
+                traverseXml(childNodes.item(i), parameters, result);
+            }
+        } catch (Exception e) {
+            e.printStackTrace(); // Handle parsing errors appropriately
+        }
+
+        return result;
+    }
+
+    private void traverseXml(Node node, List<String> parameters, Map<String, Object> result) {
+        // Process only elements (ignore text nodes, comments, etc.)
+        if (node.getNodeType() == Node.ELEMENT_NODE) {
+            // If the tag is in the specified parameters or it is a direct child of one of the parameters,
+            // add it to the result map
+            if (parameters.contains(node.getNodeName()) || parameters.contains(node.getParentNode().getNodeName())) {
+                // If the tag has child nodes, recursively process them
+                NodeList childNodes = node.getChildNodes();
+                log.info("line 127 : " + node.getNodeName() );
+                log.info("line 128 : " + childNodes.getLength() );
+                if (childNodes.getLength() > 1) {
+                    Map<String, Object> nestedResult = new HashMap<>();
+                    for (int i = 0; i < childNodes.getLength(); i++) {
+                        traverseXml(childNodes.item(i), parameters, nestedResult);
+                    }
+                    result.put(node.getNodeName(), nestedResult);
+                } else {
+                    log.info("line 135 : " + node.getTextContent().trim());
+                    // If the tag has no child nodes, add its text content to the result map
+                    result.put(node.getNodeName(), node.getTextContent().trim());
+                }
+            } else {
+                // If the tag is not in the specified parameters, recursively process child nodes
+                NodeList childNodes = node.getChildNodes();
+                for (int i = 0; i < childNodes.getLength(); i++) {
+                    traverseXml(childNodes.item(i), parameters, result);
+                }
+            }
         }
     }
 }
